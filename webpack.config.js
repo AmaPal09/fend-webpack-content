317bfb3ccd72f7c81ec948bbb3c1f4cdc2cfd67c
Amarja Palekar <amarjapalekar09@gmail.com>
2021-08-03 11:27:32 -0700

    Chng: Add Html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path")
 const webpack = require("webpack")
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+
 
 module.exports = {
 	entry: './src/client/index.js',
@@ -11,6 +13,20 @@ module.exports = {
 				loader: "babel-loader"
 			}
 		]
-	}
+	},
+	plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    })
+	]
+	// plugins: [
+	// 	new HtmlWebpackPlugin(
+	// 		{
+	// 			template: "./src/client/views/index.html",
+	// 			filename: "./index.html"
+	// 		}
+	// 	)
+	// ]
 }
 
